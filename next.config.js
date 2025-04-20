d95dfeea2420286cd00daf16341ab93f130acd51
yh2072 <yh2072@nyu.edu>
2025-04-20 18:09:57 -0400

    2.4

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // 其他配置...
-  
-  // 添加运行时配置
-  runtime: {
-    // 设置特定路径为客户端组件
-    '/login': { 
-      runtime: 'edge',
-      regions: 'all' 
-    }
-  },
-  output: 'standalone', // 不使用静态导出
-  
-  // 或设置
-  // experimental: {
-  //   appDir: true
-  // }
 }
 
 module.exports = nextConfig 
