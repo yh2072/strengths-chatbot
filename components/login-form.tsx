25d997f15e4e20468f9b6b703b904910210aea99
yh2072 <yh2072@nyu.edu>
2025-04-20 18:12:47 -0400

    Update login-form.tsx

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,24 +1,24 @@
-'use client';
+// 'use client';
 
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-export default function LoginForm() {
-  const [mounted, setMounted] = useState(false);
+// export default function LoginForm() {
+//   const [mounted, setMounted] = useState(false);
   
-  // 将访问document的代码移到useEffect中
-  useEffect(() => {
-    setMounted(true);
-    // 现在可以安全访问document
-    const tag = document.createElement('script');
-    // ...其他使用document的代码
-  }, []);
+//   // 将访问document的代码移到useEffect中
+//   useEffect(() => {
+//     setMounted(true);
+//     // 现在可以安全访问document
+//     const tag = document.createElement('script');
+//     // ...其他使用document的代码
+//   }, []);
   
-  // 在组件挂载前不渲染依赖document的部分
-  if (!mounted) {
-    return <div>加载中...</div>;
-  }
+//   // 在组件挂载前不渲染依赖document的部分
+//   if (!mounted) {
+//     return <div>加载中...</div>;
+//   }
   
-  return (
-    // 组件UI
-  );
-} 
+//   return (
+//     // 组件UI
+//   );
+// } 
